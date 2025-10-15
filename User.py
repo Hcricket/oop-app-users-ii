2e1317afb57b43bf60231cd3bbc749238de2983e
HCRICKET <heinhaung91@gmail.com>
2025-10-15 16:52:32 -0400

    progressive project

--- User.py
+++ User.py
@@ -1,1 +1,11 @@
-# your improved User class goes here
+class User:
+    all_posts = []  
+
+    def __init__(self, username):
+        self.username = username
+        self.posts = []  
+
+    def create_post(self, content):
+        post = {"author": self.username, "content": content}
+        self.posts.append(post)
+        User.all_posts.append(post)
